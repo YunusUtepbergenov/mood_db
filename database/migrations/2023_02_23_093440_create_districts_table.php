57df22ca80d6fc65e7c9f1f3620ebe30cb7d2624
YunusUtepbergenov <yunusinha@gmail.com>
2023-02-23 20:25:47 +0500

    first commit

---
+++ database/migrations/2023_02_23_093440_create_districts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistrictsTable extends Migration
+{
+    protected $connection = 'pgsql';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    
+    public function up()
+    {
+        Schema::create('districts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('region_code')->unsigned();
+            $table->integer('code')->unique();
+            $table->string('name');
+            $table->timestamps();
+
+            $table->foreign('region_code')->references('code')->on('regions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('districts');
+        Schema::dropForeign('districts_region_code_foreign');
+    }
+}
